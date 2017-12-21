9057ba3d38a99d9de9a394188fe8ba22b38e8b77
gtonra89 <gtonra89@gmail.com>
2017-12-21 11:27:10 +0000

    adding getters and setter to jobID class and generate to string methods

--- src/ClientSide/jobID.java
+++ src/ClientSide/jobID.java
@@ -1,12 +1,34 @@
 package ClientSide;
 
 public class jobID {
- private int JobID = 0;
+	private int JobID = 0;
+	private String message = "";
 
-public jobID() {
-	super();
+	public jobID() {
+		super();
 
-}
- 
- 	
+	}
+
+	public int getJobID() {
+		return JobID;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String toString() {
+		return "jobID [JobID=" + JobID + ", message=" + message + "]";
+	}
+
+	public void setJobID(int jobID) {
+		JobID = jobID;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+
 }
